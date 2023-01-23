aa5e0f6d1d0fbb1bb27bfd03ba272e2babc8b0fc
NicoleM <nicole.designer58@yahoo.com>
2023-01-23 12:24:50 -0600

    commit

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String ID;
+    private String title;
+    private int YOB;
+
+
+    public Person(String firstName, String lastName, String ID, String title, int YOB) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ID = ID;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+
+    public String toString() {
+        return "Person{" +
+                "ID='" + ID + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", YOB=" + YOB +
+                '}';
+    }
 }
+// public String toCSVRecord() {
+//        return  this.IDNum + ", " + this.firstName + "," + this.lastName + "," + YOB;
+//    }
+//}
--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class PersonGenerator {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonGenerator {
+
+    public static void main(String[] args) {
+        ArrayList<String> person = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\personData.txt");
+
+        boolean done = false;
+        //ID
+        String personRec = "";
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title ="";
+        int YOB = 0;
+
+        Person sally = new Person("Sally", "Williams", 2005);
+        System.out.println(sally);
+        person.add(sally);
+        sally.setLastName("Smith");
+        System.out.println(sally);
+
+
+
+        Person fred = new Person("Fred", "Williams", 2010);
+        System.out.println(fred);
+        person.add(fred);
+
+        Person homer = new Person("Homer", "Simpson", 1975);
+        System.out.println(homer);
+        person.add(homer);
+
+        do {
+
+
+            ID = SafeInput.getNonZeroLenString(in, "Enter the ID [6 digits] ");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter first name ");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter last name ");
+            title = SafeInput.getNonZeroLenString(in,"Enter title ");
+            YOB = SafeInput.getRangedInt(in, "Enter the year of birth: ", 1000, 9999);
+
+            personRec = ID + "," + firstName + "," + lastName + "," + title + "," + YOB;
+            person.add(personRec);
+
+            done = SafeInput.getYNConfirm(in, "Are you done? ");
+
+        } while(!done);
+
+
+
+        //Create new file
+
+        try {
+
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            for (String recs : person) {
+                writer.write(recs, 0, recs.length());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
+
+// public String toCSVRecord() {
+//        return  this.IDNum + ", " + this.firstName + "," + this.lastName + "," + YOB;
+//    }
+//}
